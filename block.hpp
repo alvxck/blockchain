a2dff79c91487d45d6e8dc43461bda52d388599b
Alex Carvalho <alexander_sck@live.ca>
2023-03-03 13:10:32 -0500

    feat: create block header

---
+++ block.hpp
@@ -0,0 +1,27 @@
+#ifndef BLOCKCHAIN_BLOCK_HPP
+#define BLOCKCHAIN_BLOCK_HPP
+
+#include <cstdint>
+#include <iostream>
+
+using namespace std;
+
+class Block {
+    private:
+        uint32_t _nIndex;
+        int64_t _nNonce;
+        string _sData;
+        string _sHash;
+        time_t _tTime;
+
+        string _CalculateHash() const;
+
+    public:
+        string sPrevHash;
+        Block (uint32_t nIndexIn, const string &sDataIn);
+        string GetHash();
+        void MideBlock(uint32_t nDifficulty);
+
+};
+
+#endif
