946ba7a8df0420bf9e571eabaf2fd86d51016e36
Alex Carvalho <alexander_sck@live.ca>
2023-03-04 10:06:35 -0500

    feat: implement blockchain source code

---
+++ Blockchain.cpp
@@ -0,0 +1,19 @@
+#include "Blockchain.hpp"
+
+// Initialize blockchain with genesis block
+Blockchain::Blockchain() {
+    _vChain.emplace_back(Block(0, "Genesis Block"));
+    _nDifficulty = 5;
+}
+
+// Add block to blockchain
+void Blockchain::AddBlock(Block bNew) {
+    bNew.sPrevHash = _GetLastBlock().GetHash();
+    bNew.MineBlock(_nDifficulty);
+    _vChain.push_back(bNew);
+}
+
+// Returns last block in blockchain
+Block Blockchain::_GetLastBlock() const {
+    return _vChain.back();
+}
